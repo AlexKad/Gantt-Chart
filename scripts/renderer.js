08f13801e1e89ef3efc903e71890c99847ba7056
Alex Orlova <akad.alex@gmail.com>
2017-07-28 15:06:08 -0700

    Render grid axis for the chart based on data

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,4 +1,4 @@
-const barHeight = 90;
+const barHeight = 60;
 const chartMargin = {top: 40, right: 20, bottom: 20, left: 40};
 const leftPadding = 60;
 
@@ -13,18 +13,18 @@ function renderDateAxis(scale, svg, height){
                 .scale(scale)
                 .tickSize(height);
   var g = svg.append("g")
-            .attr("class", "axis")
+            .attr("class", "axis y-axis")
             .attr("transform", "translate("+ leftPadding +", 20)")
             .call(customXAxis);
 
   function customXAxis(g) {
     g.call(xAxis);
     g.select(".domain").remove();
-    g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
-    g.selectAll(".tick text").attr("y", -15).attr("dx", 35);
+    g.selectAll(".tick:not(:first-of-type) line");
+    g.selectAll(".tick text").attr("y", -15).attr("dx", 30);
   }
 }
-function getNameScale(data, height){
+function getNameScale(data){
   var scale = d3.scaleBand()
                 .domain(data.map(function(d) { return d.name; }))
                 .range([ 0, barHeight*data.length]);
@@ -35,15 +35,15 @@ function renderNameAxis(scale, svg, width){
                 .scale(scale)
                 .tickSize(width);
   svg.append("g")
-     .attr("class", "axis")
+     .attr("class", "axis x-axis")
      .attr("transform", "translate("+ leftPadding +", 40)")
      .call(customYAxis);
 
    function customYAxis(g) {
      g.call(yAxis);
      g.select(".domain").remove();
-     g.selectAll(".tick:not(:last-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
-     g.selectAll(".tick text").attr("x", -40).attr("dy", -20);
+     g.selectAll(".tick:not(:last-of-type) line");
+     g.selectAll(".tick text").attr("x", -45).attr("dy", -20);
    }
 }
 
@@ -55,10 +55,10 @@ function renderChart(data, minDate, maxDate){
   var g = svg.append("g")
           .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
 
-  var dateScale = getDateScale(minDate, maxDate, width);
-  var nameScale = getNameScale(data, height);
+  var dateScale = getDateScale(minDate, maxDate, barHeight*(maxDate-minDate));
+  var nameScale = getNameScale(data);
 
-  renderDateAxis(dateScale, svg, height);
-  renderNameAxis(nameScale, svg, width);
+  renderDateAxis(dateScale, svg, barHeight*data.length-15);
+  renderNameAxis(nameScale, svg, barHeight*(maxDate-minDate));
 
 }
