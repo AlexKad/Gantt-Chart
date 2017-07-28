756852e5fb23f0c5b1fa0692d9d41eea921181ab
Alex Orlova <akad.alex@gmail.com>
2017-07-28 16:55:11 -0700

    Apply style for grid axis

--- scripts/chart-render.js
+++ scripts/chart-render.js
@@ -1,44 +1,64 @@
-
 const barHeight = 50;
-const chartMargin = {top: 20, right: 20, bottom: 30, left: 40};
+const chartMargin = {top: 40, right: 20, bottom: 20, left: 40};
+const leftPadding = 60;
 
-function renderDateAxis(g, width, height, minDate=0, maxDate=31){
-  var x = d3.scaleLinear().rangeRound([55, width]);
-  x.domain([minDate, maxDate]);
-
-  var xAxis = d3.axisBottom(x).tickSize(height);
-
-  g.append("g")
-      .attr("class", "axis axis-x")
-      .attr("transform", "translate(0,0)")
-      .call(customXAxis);//d3.axisTop(x));
+function getDateScale(minDate, maxDate, width){
+  var scale = d3.scaleLinear()
+                .domain([minDate, maxDate])
+                .range([0, width-leftPadding]);
+  return scale;
+}
+function renderDateAxis(scale, svg, height){
+  var xAxis = d3.axisBottom()
+                .scale(scale)
+                .tickSize(height);
+  var g = svg.append("g")
+            .attr("class", "axis x-axis")
+            .attr("transform", "translate("+ leftPadding +", 15)")
+            .call(customXAxis);
 
   function customXAxis(g) {
     g.call(xAxis);
     g.select(".domain").remove();
-    g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
-    g.selectAll(".tick text").attr("y", 4).attr("dx", 15);
+    g.selectAll(".tick text").attr("y", -15).attr("dx", 30);
+
+    var lastLine = g.select(".tick:last-of-type");
+    var t = lastLine.attr('transform');
+    var translate = t.substring(t.indexOf("(")+1, t.indexOf(")")).split(",");
+    g.append("g")
+     .attr('class', 'tick')
+     .attr("transform", "translate(" + (+translate[0]+ barHeight) + "," + (+translate[1])+")")
+     .append('line').attr("y2", height);
   }
-  return x;
 }
+function getNameScale(data){
+  var scale = d3.scaleBand()
+                .domain(data.map(function(d) { return d.name; }))
+                .range([ 0, barHeight*data.length]);
+  return scale;
+}
+function renderNameAxis(scale, svg, width){
+  var yAxis = d3.axisRight()
+                .scale(scale)
+                .tickSize(width-10);
+  svg.append("g")
+     .attr("class", "axis y-axis")
+     .attr("transform", "translate("+ leftPadding +", 40)")
+     .call(customYAxis);
 
-function renderTaskAxis(g, width, data){
-  var y = d3.scaleBand()
-            .rangeRound([ 20, barHeight*data.length])
-            .domain(data.map(function(d) { return d.name; }));
-  var yAxis = d3.axisRight(y).tickSize(width);
-  g.append("g")
-      .attr("class", "axis axis-y")
-      .attr("transform", "translate(0,0)")
-      .call(customYAxis);//d3.axisLeft(y));
+   function customYAxis(g) {
+     g.call(yAxis);
+     g.select(".domain").remove();
+     g.selectAll(".tick text").attr("x", -45).attr("dy", -20);
 
-  function customYAxis(g) {
-    g.call(yAxis);
-    g.select(".domain").remove();
-    g.selectAll(".tick:not(:last-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
-    g.selectAll(".tick text").attr("x", 4).attr("dy", -4);
-  }
-  return y;
+     var lastLine = g.select(".tick:first-of-type");
+     var t = lastLine.attr('transform');
+     var translate = t.substring(t.indexOf("(")+1, t.indexOf(")")).split(",");
+     g.insert("g",":first-child")
+      .attr('class', 'tick')
+      .attr("transform", "translate(" + (+translate[0]) + "," + (+translate[1]-barHeight)+")")
+      .append('line').attr("x2", width-10);
+   }
 }
 
 function renderChart(data, minDate, maxDate){
@@ -49,16 +69,21 @@ function renderChart(data, minDate, maxDate){
   var g = svg.append("g")
           .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
 
-  var x = renderDateAxis(g, width, height, minDate, maxDate);
-  var y = renderTaskAxis(g, width, data);
+  var dateAxisWidth = barHeight*(maxDate-minDate) + barHeight;
+  var nameAxisHeight = barHeight*data.length;
 
+  var dateScale = getDateScale(minDate, maxDate, dateAxisWidth);
+  var nameScale = getNameScale(data);
+
+  renderDateAxis(dateScale, svg, nameAxisHeight);
+  renderNameAxis(nameScale, svg, dateAxisWidth);
 
   g.selectAll(".bar")
       .data(data)
       .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", function(d) { return x(d.start); })
-        .attr("y", function(d) { return y(d.name); })
-        .attr("width", function(d) { return x(d.end)-x(d.start); })
-        .attr("height", barHeight/2);
+        .attr("x", function(d) { return dateScale(d.start)+20; })
+        .attr("y", function(d) { return nameScale(d.name)-25; })
+        .attr("width", function(d) { return dateScale(d.end+1)-dateScale(d.start); })
+        .attr("height", barHeight);
 }
--- scripts/tasks-manager.js
+++ scripts/tasks-manager.js
@@ -1,7 +1,8 @@
 
 var tasks = [ { name: 'task 1', start: 1, end: 4 },
               { name: 'task 2', start: 2, end: 7 },
-              { name: 'task 3', start: 5, end: 12} ];
+              { name: 'task 3', start: 5, end: 12} ,
+              { name: 'taks 4', start: 6, end: 15}];
 
 
 var minDate = Math.min.apply(null, tasks.map(el => el.start));
