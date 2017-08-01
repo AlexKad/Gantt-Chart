ac503385f2ed6180c2b64cb4d32f9837c5927440
Alex Orlova <akad.alex@gmail.com>
2017-07-31 18:14:26 -0700

    Add window for editing tasks

--- scripts/tasks-manager.js
+++ scripts/tasks-manager.js
@@ -1,4 +1,6 @@
-
+const editWnd = $('#editTaskWnd');
+const editForm = $('#editForm');
+const mask = $('.mask');
 var tasks = [ { name: 'task 1', start: 1, end: 4 },
               { name: 'task 2', start: 2, end: 7 },
               { name: 'task 3', start: 5, end: 12} ,
@@ -9,3 +11,19 @@ var minDate = Math.min.apply(null, tasks.map(el => el.start));
 var maxDate = Math.max.apply(null, tasks.map(el => el.end));
 
 renderChart(tasks, minDate, maxDate);
+
+function showAddTaskWnd(){
+  editWnd.show();
+  mask.show();
+}
+
+function addTask(){
+  //TODO: read data and push to tasks
+  closeEditWnd();
+  editForm.trigger('reset');
+}
+
+function closeEditWnd(){
+  editWnd.hide();
+  mask.hide();
+}
